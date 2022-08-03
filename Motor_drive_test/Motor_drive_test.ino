dcd785b06c7b48a675916846b8afeb45dd9fbc28
KoKoLates <the21515@gmail.com>
2022-08-03 22:06:40 +0800

    modify the pin define and adding the comments

--- Motor_drive_test/Motor_drive_test.ino
+++ Motor_drive_test/Motor_drive_test.ino
@@ -1,31 +1,33 @@
- # define LLPWM 2
- # define LRPWM 3
- # define RRPWM 4
- # define RLPWM 5
- # define LLEN 22
- # define LREN 23
- # define RLEN 24
- # define RREN 25
+ # define LMotor_LPWM 2
+ # define LMotor_RPWM 3
+ # define RMotor_RPWM 4
+ # define RMotor_LPWM 5
+ # define LMotor_LEN 22
+ # define LMotor_REN 23
+ # define RMotor_LEN 24
+ # define RMotor_REN 25
 
  void setup(){
-  pinMode(LLPWM, OUTPUT);
-  pinMode(LRPWM, OUTPUT);
-  pinMode(RRPWM, OUTPUT);
-  pinMode(RLPWM, OUTPUT);
-  pinMode(LLEN, OUTPUT);
-  pinMode(LREN, OUTPUT);
-  pinMode(RLEN, OUTPUT);
-  pinMode(RREN, OUTPUT);
-  
-  digitalWrite(LLEN, HIGH);
-  digitalWrite(LREN, HIGH);
-  digitalWrite(RREN, HIGH);
-  digitalWrite(RLEN, HIGH);
+  pinMode(LMotor_LPWM, OUTPUT);
+  pinMode(LMotor_RPWM, OUTPUT);
+  pinMode(RMotor_RPWM, OUTPUT);
+  pinMode(RMotor_LPWM, OUTPUT);
+  pinMode(LMotor_LEN, OUTPUT);
+  pinMode(LMotor_REN, OUTPUT);
+  pinMode(RMotor_LEN, OUTPUT);
+  pinMode(RMotor_REN, OUTPUT);
+
+  // Pull the LEN to high volatage
+  digitalWrite(LMotor_LEN, HIGH);
+  digitalWrite(LMotor_REN, HIGH);
+  digitalWrite(RMotor_REN, HIGH);
+  digitalWrite(RMotor_LEN, HIGH);
  }
 
 void loop(){
-  analogWrite(LLPWM, 200);
-  analogWrite(LRPWM, 0);
-  analogWrite(RRPWM, 0);
-  analogWrite(RLPWM, 200);
+  // Moving Forward
+  analogWrite(LMotor_LPWM, 100);
+  analogWrite(LMotor_RPWM, 0);
+  analogWrite(RMotor_RPWM, 100);
+  analogWrite(RMotor_LPWM, 0);
 }
